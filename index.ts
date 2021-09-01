438d04a9c27a809c71e9c99ea91c4286fea0e3c2
Marcin Czerniak <marcin.czerniak@idevelopment.pl>
2021-09-01 21:25:41 +0200

    feat: Package initialization

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { ReactiveAbstract } from './types/reactive-abstract.type';
+import { ReactiveGroup } from './classes/ractive-group.class';
+import { ReactiveArray } from './classes/reactive-array.class';
+import { ReactiveControl } from './classes/reactive-control.class';
+
+export { ReactiveAbstract, ReactiveGroup, ReactiveArray, ReactiveControl };
